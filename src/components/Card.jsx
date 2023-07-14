222ffbaf98d919ff9a8075c1039915ea001d9941
Shashank keshri <99127554+Shashankkeshri91@users.noreply.github.com>
2023-07-14 12:50:08 +0530

    Split the other component  into Detail and Avatar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import Detail from "./Detail";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar seprateImg = {props.img}/>
+        {/* <img className="circle-img" src={props.img} alt="avatar_img" /> */}
       </div>
       <div className="bottom">
-        <p className="info">{props.phone}</p>
-        <p className="info">{props.email}</p>
+        <Detail detailInfo = {props.phone}/>
+        <Detail detailInfo = {props.email}/>
       </div>
     </div>
   );
